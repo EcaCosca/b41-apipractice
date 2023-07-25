e388f676bb49ef83593089573274c2e6bbc053ad
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-25 11:08:48 +0200

    Example with fetch done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // HERE I STORE THE INFORMATION
+  const [data, setData] = useState([])
+
+  // THIS WILL DICTATE WHEN WE WANT TO GET THE INFORMATION 
+  useEffect(()=>{
+    getData()
+    console.log(data);
+  }, [])
+
+  // THIS FUNCTION IS IN CHARGE OF GETTING THE INFORMATION WHENEVER IT'S CALLED UPON AND STORE IT IN THE STATE "data"
+  const getData = () => {
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>{
+              setData(json)
+              // console.log(json);
+              })
+            .catch(err => console.log(err))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        data ?
+        data.map(element => 
+          <div id={element.id}>
+            <h2>{element.title}</h2>
+            <p>{element.description}</p>
+            <h4>PRICE: {element.price}</h4>
+            <img src={element.image}/>
+          </div>)
+          :
+          <h3>LOADING</h3>
+      }
     </>
   )
 }
