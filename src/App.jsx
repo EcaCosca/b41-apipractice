3f160aade12d68d1294d56b68f2764f8b5a59aa4
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-25 11:16:28 +0200

    Example for single id done

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,38 @@ function App() {
 
   // THIS WILL DICTATE WHEN WE WANT TO GET THE INFORMATION 
   useEffect(()=>{
-    getData()
-    console.log(data);
+    getAllData()
+    // console.log(data);
   }, [])
 
   // THIS FUNCTION IS IN CHARGE OF GETTING THE INFORMATION WHENEVER IT'S CALLED UPON AND STORE IT IN THE STATE "data"
-  const getData = () => {
+  const getAllData = () => {
     fetch('https://fakestoreapi.com/products')
-            .then(res=>res.json())
-            .then(json=>{
-              setData(json)
-              // console.log(json);
-              })
-            .catch(err => console.log(err))
+      .then(res=>res.json())
+      .then(json=>{
+        setData(json)
+        // console.log(json);
+        })
+      .catch(err => console.log(err))
+  }
+
+  const getSingleData = (id) => {
+    fetch(`https://fakestoreapi.com/products/${id}`)
+      .then(res=>res.json())
+      .then(json=>{
+        let payload = [json]
+        setData(payload)
+        // console.log(payload);
+        })
+      .catch(err => console.log(err))
   }
 
   return (
     <>
+      <h2>Get all products</h2>
+      <button onClick={getAllData}>Click here</button>
+      <h2>Get a single product by ID</h2>
+      <input type="text" onChange={(event)=>getSingleData(Number(event.target.value))}></input>
       {
         data ?
         data.map(element => 
