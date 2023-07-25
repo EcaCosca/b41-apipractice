d4bfa452a1675b77ca618486c94de4af226da6ae
EcaCosca <enriquecoscarelli@gmail.com>
2023-07-25 11:42:07 +0200

    DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,6 +7,13 @@ import './App.css'
 function App() {
   // HERE I STORE THE INFORMATION
   const [data, setData] = useState([])
+  const [newItem, setItem] = useState({
+    title: 'Batch 41 magic bullet blender from the future',
+    price: 129.5,
+    description: ' If you send an object like the code above, it will return you an object with a new id. remember that nothing in real will insert into the database. so if you want to access the new id you will get a 404 error.',
+    image: 'https://i.pravatar.cc',
+    category: 'electronic'
+})
 
   // THIS WILL DICTATE WHEN WE WANT TO GET THE INFORMATION 
   useEffect(()=>{
@@ -25,14 +33,54 @@ function App() {
   }
 
   const getSingleData = (id) => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-      .then(res=>res.json())
-      .then(json=>{
-        let payload = [json]
-        setData(payload)
-        // console.log(payload);
-        })
-      .catch(err => console.log(err))
+    // USING FETCH 
+    // fetch(`https://fakestoreapi.com/products/${id}`)
+    //   .then(res=>res.json())
+    //   .then(json=>{
+    //     let payload = [json]
+    //     setData(payload)
+    //     // console.log(payload);
+    //     })
+    //   .catch(err => console.log(err))
+
+    // USING AXIOS 
+    axios(`https://fakestoreapi.com/products/${id}`).then(res => setData([res.data]))
+  }
+
+  const CreateNewItem = () => {
+    let payload = {
+      title: 'Batch 41 magic bullet blender from the future',
+      price: 129.5,
+      description: ' If you send an object like the code above, it will return you an object with a new id. remember that nothing in real will insert into the database. so if you want to access the new id you will get a 404 error.',
+      image: 'https://i.pravatar.cc',
+      category: 'electronic'
+  }
+
+  // payload = JSON.stringify(payload);
+
+    // fetch('https://fakestoreapi.com/products',{
+    //   method: "POST",
+    //   header: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: payload,
+    // })
+    // .then(res => {
+    //   res.json()
+    // })
+    // .then(data => {
+    //   console.log(data);
+
+    //   setData([data])
+    // })
+    // .catch(error => console.log(error))
+
+    axios({
+      method: 'post',
+      url: 'https://fakestoreapi.com/products',
+      data: payload
+    }).then(res => setData([res.data]))
+
   }
 
   return (
@@ -41,6 +89,8 @@ function App() {
       <button onClick={getAllData}>Click here</button>
       <h2>Get a single product by ID</h2>
       <input type="text" onChange={(event)=>getSingleData(Number(event.target.value))}></input>
+      <h2>Create a new product</h2>
+      <button onClick={CreateNewItem}>Click here</button>
       {
         data ?
         data.map(element => 
